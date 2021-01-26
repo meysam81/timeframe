076f8b257de0d68a172470541161e5f0e4d64d7c
Meysam Azad <MeysamAzad81@yahoo.com>
2021-01-26 20:46:56 +0330

    setup.py: read the package info from the package itself, and add support for py3.6+

--- setup.py
+++ setup.py
@@ -1,23 +1,30 @@
 import setuptools
+import timeframe
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-PACKAGES = setuptools.find_packages("src")
+PACKAGES = setuptools.find_packages(include=("timeframe",))
 
 setuptools.setup(
-    name="timeframe",
-    version="0.1.0",
-    author="Meysam Azad",
-    author_email="MeysamAzad81@gmail.com",
-    description="Time Frame",
+    name=timeframe.__name__,
+    version=timeframe.__version__,
+    author=timeframe.__author__,
+    author_email=timeframe.__author_email__,
+    description="Calculation of time frames using the built-in datetime module",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/meysam81/timeframe",
     packages=PACKAGES,
     classifiers=[
-        "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
-    python_requires=">=3.9",
+    python_requires=">=3.6",
 )
