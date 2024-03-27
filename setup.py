ac299aac56f4dde5c8a6cfd99d08200fb56948ba
Meysam Azad <MeysamAzad81@gmail.com>
2024-03-27 19:35:09 +0700

    feat: add adoption and tests for python3.12

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.7,<3.13",
 )
