b402056f3419058789019501539dfd26e3c367b6
Meysam Azad <meysam@developer-friendly.blog>
2024-10-28 10:21:40 +0700

    chore: test for py3.13

--- setup.py
+++ setup.py
@@ -33,6 +33,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
     ],
-    python_requires=">=3.7,<3.13",
+    python_requires=">=3.7, <4",
 )
